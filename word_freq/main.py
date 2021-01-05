e8ca28c943a31fe979112f31dd90d6bcbc37a0bd
Furkan.Ozdemir <ozthefur@gmail.com>
2021-01-05 14:21:19 +0300

    google auth completed

--- word_freq/main.py
+++ word_freq/main.py
@@ -2,10 +2,6 @@ import wget, requests, os
 from bs4 import BeautifulSoup
 
 
-# TODO
-# sayfadaki tüm yazıları alıcak ve bir txt dosyasına kaydedicek
-# pdf olarak indirmeyecek yani
-# p h1 h2 a'taglerinin içindeki textleri
 
 def download_book(book_name):
     url = "https://en.wikibooks.org/wiki/" + book_name
@@ -30,6 +26,7 @@ def download_book(book_name):
 def word_freq_single(book_name):
     words = []  # aynı kelimeler var
     words2 = []  # aynı kelimeler yok
+    print("Book Name : " + book_name)
     with open(book_name + ".txt", "r", encoding="utf-8") as file:
         for line in file:
             for word in line.split():
@@ -43,13 +40,23 @@ def word_freq_single(book_name):
             print(word + " " + str(words.count(word)))
 
 
-# TODO kelimeleri büyükten küçüğe sırala tekrar sayılarına göre
-# bunun için kelimeyi ve kelime.countunu 2d li listte tutabilirsin belki
+
 
 # TODO stop wordsleri ignore edecek
+# Todo kaç kelime göstermek istediğini soracak
+# TODO Outputu düzenle
+#todo kitap isimleri First Aid , Latin , Algorithms , Chess
+
 def main():
-    download_book(book_name)
-    word_freq_single(book_name)
+    if book_number == 1:
+        download_book(book_name)
+        word_freq_single(book_name)
+    elif book_number == 2:
+        download_book(book_name)
+        download_book(book_name2)
+        word_freq_single(book_name)
+        print("#######################################")
+        word_freq_single(book_name2)
 
 
 headers = {
@@ -59,6 +66,16 @@ headers = {
     'Access-Control-Max-Age': '3600',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 }
-print("Please enter a book name (Case sensitive): ", end="")
-book_name = input()
-main()
+print("How many books do you want to download: (type 1 or 2 ) :  ")
+book_number = int(input())
+book_name2 = ""
+if book_number == 1:
+    print("Please enter a book name (Case sensitive): ", end="")
+    book_name = input()
+    main()
+elif book_number == 2:
+    print("Please enter the first books name(Case sensitive): ", end="")
+    book_name = input()
+    print("Please enter the second books name(Case sensitive): ", end="")
+    book_name2 = input()
+    main()
